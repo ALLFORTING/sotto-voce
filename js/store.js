aba8de412909fb31fc7be6fd97592761258c60ca
ALLFORTING <auauauting1028@gmail.com>
2026-07-08 07:11:44 +0800

    Add attachment timestamp CSS fallback

--- js/store.js
+++ js/store.js
@@ -1,4 +1,4 @@
-export const VERSION = "cheng-v160";
+export const VERSION = "cheng-v161";
 
 const readJson = (key, fallback) => {
   try {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "cheng-v160";
+const VERSION = "cheng-v161";
 const CACHE_PREFIX = "cheng-static-";
 const CACHE = `${CACHE_PREFIX}${VERSION}`;
 const STATIC = [
